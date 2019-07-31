0e436ef767865a09b77345512e57b85bc59548fc
Thomasmaghanga <wavyylikethesea@gmail.com>
2019-07-31 19:03:47 +0300

    Insert navBar colour change function

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$(window).scroll(function() {
+  	if($(document).scrollTop() > 10) {
+    	$('#nav').addClass('change');
+    }
+    else {
+    $('#nav').removeClass('change');
+    }
+  });
+});
